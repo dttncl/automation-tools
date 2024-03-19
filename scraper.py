8f7a2b658c686f756942df46f5fce9be5ed51fe6
dttncl <82695034+dttncl@users.noreply.github.com>
2024-03-19 12:57:32 -0400

    added email and phone regex

--- scraper.py
+++ scraper.py
@@ -3,9 +3,44 @@
 import re, pyperclip
 
 # TODO: Create regex for phone numbers
+
+phoneRegex = re.compile(r'''
+
+# 415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
+
+(((\d{3})|(\(\d{3}\)))?        # area code (optional)
+(-|\s)                        # first separator
+\d{3}                       # first 3 digits
+-                             # separator
+\d{4}                       # last 4 digits
+(((ext(\.)?\s)|x)             # extension (optional)
+(\d{2,5}))?)
+''',re.VERBOSE)
+
+
 # TODO: Create regex for  email addresses
+emailRegex = re.compile(r'''
+
+# sample.+_email@something.com
+
+[a-zA-Z0-9.+_]+        # name
+@                        # @ symbol
+[a-zA-Z0-9.+_]+        # domain name
+''',re.VERBOSE)
 # TODO: Get text off the clipboard
+text = pyperclip.paste()
+
 # TODO: Extract email/phone from this text
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+for phone in extractedPhone:
+    allPhoneNumbers.append(phone[0])
+
+print(allPhoneNumbers)
+print(extractedEmail)
+
 # TODO: Copy the extracted email/phone to the clipboard
 
 
