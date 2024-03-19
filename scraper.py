78ee60ef6aca933e8648d0ae9e89373fc9972db7
dttncl <82695034+dttncl@users.noreply.github.com>
2024-03-19 12:26:48 -0400

    initialize scraper

---
+++ scraper.py
@@ -0,0 +1,12 @@
+#! python3
+
+import re, pyperclip
+
+# TODO: Create regex for phone numbers
+# TODO: Create regex for  email addresses
+# TODO: Get text off the clipboard
+# TODO: Extract email/phone from this text
+# TODO: Copy the extracted email/phone to the clipboard
+
+
+
