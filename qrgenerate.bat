a789c9d9f917e5a75abe3e551d86657cda2270e6
dttncl <82695034+dttncl@users.noreply.github.com>
2024-09-06 12:23:13 -0400

    updated qrgenerate

--- qrgenerate.bat
+++ qrgenerate.bat
@@ -1,2 +1,2 @@
-@py FILE-PATH\qrgenerate.py %*
+@py C:\Users\dette\source\repos\passion-projects\python-tests\tools\qrgenerate.py %*
 @pause
--- qrgenerate.py
+++ qrgenerate.py
@@ -1,94 +1,91 @@
 #! python3
 
 """
-
     QR CODE GENERATOR
     bnhf
     
     v1: 09/06/24
-    
 """
 
 import pandas as pd
-import segno, argparse, os
+import segno, argparse, os, sys
 
 
 # TODO: Generate QR codes from csv file and save to directory
 def generate_qr(csv_file, output_dir):
-   try:
-      df = pd.read_csv(csv_file, header=None)
+    try:
+        df = pd.read_csv(csv_file, header=None)
       
-      if len(df.columns) != 1:
-      	raise ValueError("Incorrect Format: CSV file must contain exactly one column")
+        if len(df.columns) != 1:
+            raise ValueError("Incorrect Format: CSV file must contain exactly one column")
       
-      print("\nGenerating QR Codes...\n")
-      total_qrs = qr_success = qr_fail = 0
-      results = []
+        print("\nGenerating QR Codes...\n")
+        total_qrs = qr_success = qr_fail = 0
+        results = []
 
-      for index, row in df.iterrows():
-      	total_qrs += 1
+        for index, row in df.iterrows():
+            total_qrs += 1
       	
-      	data = row[df.columns[0]]
-      	img_path = os.path.join(output_dir, f"{data}.png")
+            data = row[df.columns[0]]
+            img_path = os.path.join(output_dir, f"{data}.png")
       	
-      	# check duplicate entries
-      	if os.path.exists(img_path):
-           status = "FAILED: Duplicate file exists"
-      	   qr_fail += 1
-      	   results.append({"data": data, "status": status})
-      	   print(f"{data} - {status}")
-      	   continue
+            # check duplicate entries
+            if os.path.exists(img_path):
+                status = "FAILED: Duplicate file exists"
+                results.append({"data": data, "status": status})
+                qr_fail += 1
+                print(f"{data} - {status}")
+                continue
       	
-      	try:
-           # create qr
-      	   qr = segno.make_qr(data)
-      	   qr.save(img_path, scale=10)
-      	   status = "SUCCESS"
-      	   qr_success += 1
+            try:
+                # create qr
+                qr = segno.make_qr(data)
+                qr.save(img_path, scale=15)
+                status = "SUCCESS"
+                qr_success += 1
       		
-      	except Exception as e:
-      	   status = "FAILED: Invalid characters"
-      	   qr_fail += 1
+            except Exception as e:
+                status = "FAILED: Invalid characters"
+                qr_fail += 1
       	
-      	results.append({"data": data, "status": status})
-      	print(f"{data} - {status}")
+            results.append({"data": data, "status": status})
+            print(f"{data} - {status}")
       
 
-      # TODO: Create a csv log file
-      log_path = os.path.join(output_dir, "logs.csv")
-      results_df = pd.DataFrame(results)
-      results_df.to_csv(log_path, index=False)
+        # TODO: Create a csv log file
+        log_path = os.path.join(output_dir, "logs.csv")
+        results_df = pd.DataFrame(results)
+        results_df.to_csv(log_path, index=False)
       
-      print(f"\n{total_qrs} entries found")
-      print(f"{qr_success} QR codes created successfully")
-      print(f"{qr_fail} QR codes failed")
+        print(f"\n{total_qrs} entries found")
+        print(f"{qr_success} QR codes created successfully")
+        print(f"{qr_fail} QR codes failed")
       
-   except ValueError as e:
-      print(f"\n\t{e}")
-      exit(1) 
+    except ValueError as e:
+        print(f"\n\t{e}")
+        exit(1) 
 		
 if __name__ == "__main__":
-      """
-      parser = argparse.ArgumentParser(description="Generate QR codes from a CSV file.")
-      parser.add_argument('csv_file', type=str, help='Path to the input CSV file.')
-      parser.add_argument('output_dir', type=str, help='Directory to save QR codes.')
+    """
+    parser = argparse.ArgumentParser(description="Generate QR codes from a CSV file.")
+    parser.add_argument('csv_file', type=str, help='Path to the input CSV file.')
+    parser.add_argument('output_dir', type=str, help='Directory to save QR codes.')
       
-      args = parser.parse_args()
+    args = parser.parse_args()
       
-      csv_file = args.csv_file
-      output_dir = args.output_dir
-      """
+    csv_file = args.csv_file
+    output_dir = args.output_dir
+    """
 
-      params = sys.argv
+    params = sys.argv
 
-      if len(params) > 1:
-         csv_file = params[0]
-         output_dir = params[1]
-         #address = ' '.join(params[1:])
+    if len(params) > 1:
+        csv_file = params[0]
+        output_dir = params[1]
+        #address = ' '.join(params[1:])
       
-      # TODO: Create output directory if it doesn't exist
-      if not os.path.exists(output_dir):
-          os.makedirs(output_dir)
+    # TODO: Create output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
       
-      generate_qr(csv_file, output_dir)
-
+    generate_qr(csv_file, output_dir)
